68af22d96e788b004faec8b51f52cdc9b62205a7
Rei Sugiura <reibomaru@gmail.com>
2021-04-13 12:07:07 +0900

    refactor: リセットボタンをmapコンポーネントに移動

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Polyline, useMapEvent } from 'react-leaflet';
 import { LatLng, LatLngExpression, LeafletMouseEvent } from 'leaflet';
 import { nanoid } from 'nanoid';
@@ -10,12 +11,6 @@ type ClickLayerProps = {
     positions: LatLng[],
     setPositions: React.Dispatch<React.SetStateAction<LatLng[]>>
 }
-
-//Todo: interface使うようにする
-type MapProps = {
-    positions: LatLng[],
-    setPositions: React.Dispatch<React.SetStateAction<LatLng[]>>
-}
   
 function ClickLayer(props: ClickLayerProps): null{
     useMapEvent('click', (e: LeafletMouseEvent)=>{
@@ -24,12 +19,20 @@ function ClickLayer(props: ClickLayerProps): null{
     return null;
 }
 
-function Map(props: MapProps){
-    const Markers: JSX.Element[] = props.positions.map((pos: LatLng): JSX.Element => {
+function Map(){
+    const [positions, setPositions] = useState<LatLng[]>([]);
+
+    const Markers: JSX.Element[] = positions.map((pos: LatLng): JSX.Element => {
         return <Marker position={[pos.lat, pos.lng]} key={nanoid()}/>
     })
-    const polyline: LatLngExpression[] = props.positions.map((pos: LatLng): LatLngExpression => [pos.lat, pos.lng])
+
+    const polyline: LatLngExpression[] = positions.map((pos: LatLng): LatLngExpression => [pos.lat, pos.lng])
+
+    function onClickHandler(): void{
+        setPositions([]);
+    }
     return(
+        <>
         <MapContainer style={{height: '600px'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
             {Markers}
             <Polyline pathOptions={limeOptions} positions={polyline}/>
@@ -37,8 +40,10 @@ function Map(props: MapProps){
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-        <ClickLayer positions={props.positions} setPositions={props.setPositions}/>
+        <ClickLayer positions={positions} setPositions={setPositions}/>
         </MapContainer>
+        <button onClick={onClickHandler}>リセット</button>
+        </>
     )
 }
 
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,18 +1,9 @@
-import { useState } from 'react';
-import { LatLng } from 'leaflet';
 import Map from '../components/Map';
 
 function Top(): JSX.Element{
-  const [positions, setPositions] = useState<LatLng[]>([]);
-
-  function onClickHandler(): void{
-    setPositions([]);
-  }
-
   return (
     <>
-      <Map positions={positions} setPositions={setPositions}/>
-      <button onClick={onClickHandler}>リセット</button>
+      <Map />
     </>
   );
 }
