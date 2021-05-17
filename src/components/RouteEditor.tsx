63519cbf80dfc2037b2ac4d8c6c0446629715e6a
Rei Sugiura <reibomaru@gmail.com>
2021-05-17 11:13:44 +0900

    feat: add insert function

--- src/components/RouteEditor.tsx
+++ src/components/RouteEditor.tsx
@@ -1,15 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FunctionComponent } from 'react';
 import { MapContainer, TileLayer, Marker, Polyline, useMapEvent } from 'react-leaflet';
-import { LatLng, LatLngExpression, LeafletMouseEvent } from 'leaflet';
+import L, { LatLng, LatLngExpression, LeafletMouseEvent } from 'leaflet';
 import { nanoid } from 'nanoid';
 import axios from 'axios'
 import 'leaflet/dist/leaflet.css';
 
 const limeOptions: {color: string} = { color: 'lime' }
 
-//Todo: interface使うようにする
 type ClickLayerProps = {
     positions: LatLng[],
+    route: string,
+    setPositions: React.Dispatch<React.SetStateAction<LatLng[]>>
+}
+
+type PolylineProps = {
+    polyline: LatLngExpression[],
     route: string,
     setPositions: React.Dispatch<React.SetStateAction<LatLng[]>>
 }
@@ -25,31 +30,37 @@ interface Response{
     points: ResponsePoint[],
     message: string
 }
+
+async function patchAdd(latitude: number, longitude: number, position: number, route: string){
+    const payload = {
+        coord:{
+            latitude: latitude,
+            longitude: longitude
+        }
+    }
+    let res;
+    try {
+        res = await axios.patch<Response>(`/routes/${route}/add/${position}`, payload);
+        return res
+    } catch (error) {
+        if(error.response.data.message){
+            console.error(error.response.data.message);
+        }
+    }
+    return res;
+}
   
 function ClickLayer(props: ClickLayerProps): null{
-    useMapEvent('click', (e: LeafletMouseEvent)=>{
-        async function patchAdd(){
-            const payload = {
-                coord:{
-                    latitude: e.latlng.lat,
-                    longitude: e.latlng.lng
-                }
-            }
-            try {
-                const res = await axios.patch<Response>(`/routes/${props.route}/add/${props.positions.length}`, payload);
-                props.setPositions(res.data.points.map((position: ResponsePoint) => new LatLng(position.latitude, position.longitude)));
-            } catch (error) {
-                if(error.response.data.message){
-                    console.error(error.response.data.message);
-                }
-            }
+    useMapEvent('click', async (e: LeafletMouseEvent)=>{
+        const res = await patchAdd(e.latlng.lat, e.latlng.lng, props.positions.length, props.route);
+        if(res){
+            props.setPositions(res.data.points.map((position: ResponsePoint) => new LatLng(position.latitude, position.longitude)));
         }
-        patchAdd()
     })
     return null;
 }
 
-function RouteEditor(props: any){
+function RouteEditor(props: any): JSX.Element{
     const [positions, setPositions] = useState<LatLng[]>([]);
 
     //Mapのルート変更時にルートを取得してpositionsを変更する
@@ -82,13 +93,46 @@ function RouteEditor(props: any){
             <Marker
                 position={[pos.lat, pos.lng]}
                 key={nanoid()}
-                eventHandlers={{click: ()=>{patchDelete(idx)}}} //todo: ここの関数を一つにまとめたい
+                eventHandlers={{click: ()=>{
+                    patchDelete(idx)}
+                }} //todo: ここの関数を一つにまとめたい
             >
             </Marker>
         )
     })
 
     const polyline: LatLngExpression[] = positions.map((pos: LatLng): LatLngExpression => [pos.lat, pos.lng])
+
+    const Polylines: FunctionComponent<PolylineProps> = (props: PolylineProps) => {
+        if(props.polyline.length){
+            let polylines: JSX.Element[] = new Array(props.polyline.length - 1);
+            for(let idx = 0; idx < props.polyline.length - 1; idx++){
+                polylines[idx] = (
+                    <Polyline
+                        pathOptions={limeOptions} 
+                        positions={[polyline[idx], polyline[idx + 1]]}
+                        key={nanoid()}
+                        eventHandlers={{click: 
+                            async (event: L.LeafletMouseEvent)=>{
+                                L.DomEvent.stopPropagation(event) //clickLayerに対してクリックイベントを送らない
+                                const res = await patchAdd(event.latlng.lat, event.latlng.lng, idx + 1, props.route)
+                                if(res){
+                                    props.setPositions(res.data.points.map((position: ResponsePoint) => new LatLng(position.latitude, position.longitude)));
+                                }
+                            }
+                        }} 
+                    />
+                )
+            }
+            return(
+                <>
+                {polylines}
+                </>
+            )
+        }else{
+            return null
+        }
+    }
 
     function onClickClearHandler(): void{
         async function patchClear(){
@@ -140,13 +184,17 @@ function RouteEditor(props: any){
         <>
         <p>現在のルート: {props.route}</p>
         <MapContainer style={{height: '600px'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-            {Markers}
-            <Polyline pathOptions={limeOptions} positions={polyline}/>
             <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-        <ClickLayer route={props.route} positions={positions} setPositions={setPositions}/>
+            {Markers}
+            <Polylines polyline={polyline} route={props.route} setPositions={setPositions}/>
+        <ClickLayer 
+            route={props.route}
+            positions={positions}
+            setPositions={setPositions}
+        />
         </MapContainer>
         {/* Todo undoできない時はボタンをdisabledにする */}
         <button onClick={onClickUndoHandler}>undo</button>
