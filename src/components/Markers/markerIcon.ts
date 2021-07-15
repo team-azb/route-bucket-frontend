9b4cbad051df5462fcd3f9f58e824eda44924cda
Rei Sugiura <reibomaru@gmail.com>
2021-07-15 15:57:09 +0900

    feat: カスタムiconを実装

---
+++ src/components/Markers/markerIcon.ts
@@ -0,0 +1,7 @@
+import L from 'leaflet'
+
+export const MarkerIcon = new L.Icon({
+    iconUrl: "flag_black_24dp.svg",
+    iconRetinaUrl: "flag_black_24dp.svg",
+    iconSize: new L.Point(25, 25),
+});
