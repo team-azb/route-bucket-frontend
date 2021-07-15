3299364ffa68112d3be1cf7b46204133efda4c60
Rei Sugiura <reibomaru@gmail.com>
2021-07-15 15:02:19 +0900

    feat: polylineをドラッグしてルートを変更できる機能を実装

--- src/components/Polylines/index.tsx
+++ src/components/Polylines/index.tsx
@@ -1,7 +1,9 @@
-import { Polyline } from "react-leaflet";
+import { useState, useRef } from "react";
+import { Polyline, Marker } from "react-leaflet";
+import { Marker as MarkerType } from "leaflet";
 import L from "leaflet";
 import { nanoid } from "nanoid";
-import { patchAdd } from "../../api/routes";
+import { patchAdd, patchMove } from "../../api/routes";
 import { Route } from "../../types";
 
 const blueOptions: { color: string } = { color: "#0000cd" };
@@ -12,34 +14,88 @@ type PolylineProps = {
   setRoute: React.Dispatch<React.SetStateAction<Route>>;
 };
 
+type TempMarkerInfo = {
+  position: L.LatLng | null;
+  index: number | null;
+};
+
 export default function Polylines(props: PolylineProps) {
-  let polylines: JSX.Element[] = new Array(props.route.segments.length);
-  for (let idx = 0; idx < props.route.segments.length; idx++) {
-    polylines[idx] = (
-      //Todo: 線の太さを上げて、線をクリックしやすくする
-      <Polyline
-        pathOptions={blueOptions}
-        positions={props.route.segments[idx]["points"].map((point) => [
-          point.latitude,
-          point.longitude,
-        ])}
-        key={nanoid()}
-        eventHandlers={{
-          click: async (event: L.LeafletMouseEvent) => {
-            L.DomEvent.stopPropagation(event); //clickLayerに対してクリックイベントを送らない
-            const res = await patchAdd(props.route.id, idx + 1, {
-              coord: {
-                latitude: event.latlng.lat,
-                longitude: event.latlng.lng,
-              },
-            });
-            if (res) {
-              props.setRoute({ ...props.route, ...res.data });
-            }
-          },
-        }}
-      />
-    );
+  const markerRef = useRef<MarkerType>(null);
+  const [tempMarkerInfo, setTempMarkerInfo] = useState<TempMarkerInfo>({
+    position: null,
+    index: null,
+  });
+
+  async function onDragMarker() {
+    const newPoint = markerRef.current?.getLatLng();
+    if (newPoint && tempMarkerInfo.index) {
+      const res = await patchMove(props.route.id, tempMarkerInfo.index, {
+        coord: {
+          latitude: newPoint.lat,
+          longitude: newPoint.lng,
+        },
+      });
+      if (res) {
+        props.setRoute({ ...props.route, ...res.data });
+        setTempMarkerInfo({ index: null, position: null });
+      }
+    }
   }
-  return <>{polylines}</>;
+
+  if (props.route.segments.length) {
+    let polylines: JSX.Element[] = new Array(props.route.segments.length);
+    for (let idx = 0; idx < props.route.segments.length; idx++) {
+      polylines[idx] = (
+        //Todo: 線の太さを上げて、線をクリックしやすくする
+        <Polyline
+          pathOptions={blueOptions}
+          positions={props.route.segments[idx]["points"].map((point) => [
+            point.latitude,
+            point.longitude,
+          ])}
+          key={nanoid()}
+          eventHandlers={{
+            click: async (event: L.LeafletMouseEvent) => {
+              L.DomEvent.stopPropagation(event); //clickLayerに対してクリックイベントを送らない
+              const res = await patchAdd(props.route.id, idx + 1, {
+                coord: {
+                  latitude: event.latlng.lat,
+                  longitude: event.latlng.lng,
+                },
+              });
+              if (res) {
+                props.setRoute({ ...props.route, ...res.data });
+              }
+            },
+            mouseover: (event) => {
+              setTempMarkerInfo({
+                ...tempMarkerInfo,
+                index: idx,
+                position: event.latlng,
+              });
+            },
+          }}
+        />
+      );
+    }
+    return (
+      <>
+        {polylines}
+        {tempMarkerInfo.position && (
+          <Marker
+            ref={markerRef}
+            draggable={true}
+            position={tempMarkerInfo.position}
+            eventHandlers={{
+              dragend: () => {
+                onDragMarker();
+              },
+            }}
+          />
+        )}
+      </>
+    );
+  } else {
+    return null;
+  }
 }
