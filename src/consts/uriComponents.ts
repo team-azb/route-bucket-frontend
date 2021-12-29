ca03ecf601ac21a88335f932a7704da7602d7490
Rei Sugiura <reibomaru@gmail.com>
2021-12-29 20:34:53 +0900

    feat: user iconのレイアウト実装

--- src/consts/uriComponents.ts
+++ src/consts/uriComponents.ts
@@ -10,6 +10,7 @@ export const pagePaths = {
   MARKER_ICON: "/icons/fmd_good_black_24dp.svg",
   GOAL_MARKER_ICON: "/icons/goal_pin.svg",
   FOCUSED_MARKER_ICON: "/icons/circle_black_24dp.svg",
+  USER_ICON: "/icons/account_circle.svg",
 };
 
 export const dynamicPathGenerator = {
