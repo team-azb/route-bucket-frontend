41f5b77f99a39520a7ea7733529c1d931fed48ec
Rei Sugiura <reibomaru@gmail.com>
2021-12-29 19:40:23 +0900

    feat: BasicInformationFieldのUI実装

--- src/components/organisms/BasicInformationTable/index.tsx
+++ src/components/organisms/BasicInformationTable/index.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { UserInfo } from "../../../types";
+import BasicInformationField from "../BasicInformationField";
+import { useSignedInUserInfoContext } from "../../../contexts/signedInUserContext";
 
 type BasicInformationTableProps = {
   userInfo: UserInfo;
 };
 
 const BasicInformationTable = ({ userInfo }: BasicInformationTableProps) => {
-  return <>{JSON.stringify(userInfo)}</>;
+  const { signedInUser } = useSignedInUserInfoContext();
+  return (
+    <>
+      <BasicInformationField
+        id="name"
+        labelName="ニックネーム"
+        fieldValue={userInfo.name}
+      />
+      <BasicInformationField
+        id="email"
+        labelName="メールアドレス"
+        fieldValue={signedInUser?.email}
+      />
+      <BasicInformationField
+        id="birthdate"
+        labelName="生年月日"
+        fieldValue={userInfo.birthdate?.toDateString()}
+      />
+    </>
+  );
 };
 
 export default BasicInformationTable;
