04a7c33234740a185b57eac8f32928c1db9728cd
Rei Sugiura <reibomaru@gmail.com>
2022-01-06 13:59:32 +0900

    feat: BasicInformationFormのvalidationを実装

--- src/components/organisms/mypage/BasicInformationUpdateForm/index.tsx
+++ src/components/organisms/mypage/BasicInformationUpdateForm/index.tsx
@@ -1,9 +1,10 @@
 import React, { ChangeEvent, useCallback, useMemo, useState } from "react";
-import { UserInfo } from "../../../../types";
+import { UserInfo, ValidationMessages } from "../../../../types";
 import IconImageUpload from "../IconImageUpload";
 import FormField from "../../../atoms/FormField";
 import InputWithError from "../../../molecules/InputWithError";
 import styles from "./style.module.css";
+import { Fields, Form, validateAndGetMessages } from "./helper";
 
 type BasicInformationUpdateFormProps = {
   exitEditingModeHandler: () => void;
@@ -14,16 +15,36 @@ const BasicInformationUpdateForm = ({
   exitEditingModeHandler,
   userInfo,
 }: BasicInformationUpdateFormProps) => {
-  const [userInfoForm, setUserInfoForm] = useState<UserInfo>(userInfo);
+  const [userInfoForm, setUserInfoForm] = useState<Form>(userInfo as Form);
+  const [validatonMessages, setValidatonMessages] =
+    useState<ValidationMessages>();
   const [previewFile, setPreviewFile] = useState<File>();
   const previewUrl = useMemo(() => {
-    return previewFile
-      ? URL.createObjectURL(previewFile)
-      : userInfoForm.icon_url;
-  }, [previewFile, userInfoForm.icon_url]);
+    return previewFile ? URL.createObjectURL(previewFile) : userInfo.icon_url;
+  }, [previewFile, userInfo.icon_url]);
+
+  const asyncUpdatgeValidationMessages = async (
+    fieldName: Fields,
+    value: string,
+    prevForm: Form
+  ) => {
+    const result = await validateAndGetMessages(fieldName, value, prevForm);
+    setValidatonMessages((prevState) => {
+      return {
+        ...prevState,
+        ...result,
+      };
+    });
+  };
+
   const changeFormHandler = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
       setUserInfoForm((prevForm) => {
+        asyncUpdatgeValidationMessages(
+          event.target.name as Fields,
+          event.target.value,
+          prevForm
+        );
         return {
           ...prevForm,
           [event.target.name]: event.target.value,
@@ -52,7 +73,7 @@ const BasicInformationUpdateForm = ({
               type="text"
               value={userInfoForm.name}
               onChange={changeFormHandler}
-              errorMessage=""
+              errorMessage={validatonMessages?.name}
             />
           </FormField>
           <FormField className={styles.field}>
@@ -63,7 +84,7 @@ const BasicInformationUpdateForm = ({
               type="date"
               value={userInfoForm.birthdate}
               onChange={changeFormHandler}
-              errorMessage=""
+              errorMessage={validatonMessages?.birthdate}
             />
           </FormField>
           <button className={styles.submitButton}>更新</button>
